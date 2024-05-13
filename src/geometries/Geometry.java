237740f05ca8093bdbcc7c056ba52cfee9a81f3d
shira kernberg <kernberg@g.jct.ac.il>
2024-05-13 14:57:39 +0300

    stage1 without commentary

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -3,6 +3,9 @@ package geometries;
 import primitives.Point;
 import primitives.Vector;
 
+/**
+ * interface for geometric shapes
+ */
 public interface Geometry {
     public Vector getNormal(Point p);
 }
